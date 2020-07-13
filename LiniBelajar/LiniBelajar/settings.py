ff5d67cf646032c5a96eb75c292bd7bbc7079406
Acer <oktoberlin@gmail.com>
2020-07-13 20:32:00 +0700

    add database postres

--- LiniBelajar/LiniBelajar/settings.py
+++ LiniBelajar/LiniBelajar/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'LiniBelajar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'linibelajar',
+        'USER': 'okto',
+        'PASSWORD': 'okto2okE',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -121,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
