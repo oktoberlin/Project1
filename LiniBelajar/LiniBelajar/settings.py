9130d4a8653eea9dd2135103c23dd9552d419d40
Acer <oktoberlin@gmail.com>
2020-07-15 22:02:11 +0700

    add crispy forms

--- LiniBelajar/LiniBelajar/settings.py
+++ LiniBelajar/LiniBelajar/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['167.99.64.21', 'linibelajar.com',
 INSTALLED_APPS = [
     'home',
     'user',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -141,3 +142,5 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
