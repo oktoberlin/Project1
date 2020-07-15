935851288cc2f2b231ab641294f2986a57fc7d50
Acer <oktoberlin@gmail.com>
2020-07-15 13:07:08 +0700

    remove postgres from computer

--- LiniBelajar/LiniBelajar/settings.py
+++ LiniBelajar/LiniBelajar/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '$i-+k7dqa8hj45!7-)-s7ti5&tlm5m)w3!jz^^u!xysu0il(yw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['167.99.64.21', 'linibelajar.com', 'www.linibelajar.com']
+ALLOWED_HOSTS = ['167.99.64.21', 'linibelajar.com',
+                 'www.linibelajar.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -78,13 +79,10 @@ WSGI_APPLICATION = 'LiniBelajar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'linibelajar',
-        'USER': 'okto',
-        'PASSWORD': 'okto2okE',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+
 }
 
 
@@ -125,12 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'static')
-#]
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
-#try:
-#    from .local_settings import *
-#except ImportError:
-#    pass
+try:
+    from .local_settings import *
+except ImportError:
+    pass
