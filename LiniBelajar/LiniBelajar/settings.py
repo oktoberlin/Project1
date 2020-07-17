434bd78aa710aa46d94badba9617af7db85effe5
Acer <oktoberlin@gmail.com>
2020-07-17 10:36:11 +0700

    success to add profile pics

--- LiniBelajar/LiniBelajar/settings.py
+++ LiniBelajar/LiniBelajar/settings.py
@@ -139,6 +139,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 try:
     from .local_settings import *
 except ImportError:
