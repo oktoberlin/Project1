0994ce7bf18621a75440469a0a2041df76e8fc09
Acer <oktoberlin@gmail.com>
2020-07-19 07:55:24 +0700

    finsih password reset and complete

--- LiniBelajar/LiniBelajar/settings.py
+++ LiniBelajar/LiniBelajar/settings.py
@@ -175,3 +175,10 @@ LOGIN_REDIRECT_URL = 'community'
 LOGIN_URL = 'login'
 
 SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'oktoberlin@gmail.com'
+EMAIL_HOST_PASSWORD = 'okto2bebetteR'
--- LiniBelajar/LiniBelajar/urls.py
+++ LiniBelajar/LiniBelajar/urls.py
@@ -32,11 +32,13 @@ urlpatterns = [
         template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(
-        template_name='password_reset.html'), name='password-reset'),
-    path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='password_reset_confirm.html'), name='password-reset-confirm'),
-    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(
-        template_name='password_reset_done.html'), name='password-reset-done'),
+        template_name='password_reset.html'), name='password_reset'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-done', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-complete', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
