07e2e69c779ec71c660017ac9c5464835ff88206
Acer <oktoberlin@gmail.com>
2020-07-26 18:26:15 +0700

    'fix_user_profile'

--- LiniBelajar/community/views.py
+++ LiniBelajar/community/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView, DetailView, CreateView, UpdateView, DeleteView)
 from .models import Post
+from user.models import Profile
+from user.forms import RegisterUserForm, UserUpdateForm, ProfileUpdateForm
 
 
 def community(request):
@@ -19,6 +22,18 @@ class PostListView(ListView):
     template_name = 'community.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+
+class UserProfileView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+    def get_query_set(self):
+        user = get_object_or_404(User, username=self.kwargs.get(
+            'username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(LoginRequiredMixin, DetailView):
