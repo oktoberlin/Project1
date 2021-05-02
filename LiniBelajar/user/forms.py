ba486027a44be59d807c537b70d04f93dd1791f5
oktoberlin <oktoberlin@gmail.com>
2021-05-02 20:04:12 +0700

    add_disclaimer

--- LiniBelajar/user/forms.py
+++ LiniBelajar/user/forms.py
@@ -8,11 +8,15 @@ class RegisterUserForm(UserCreationForm):
     email = forms.EmailField()
     first_name = forms.CharField()
     last_name = forms.CharField()
+    terms = forms.BooleanField(
+        error_messages={'required': 'Persyaratan dan Kondisi belum dicentang.'},
+        label = "Disclaimer: Dengan memilih centang, anda menyetujui bahwa konten yang anda bagikan valid dan dipertanggungjawabkan penuh oleh akun anda. Linibelajar tidak bertanggungjawab atas ketidakvalidan info yang anda bagikan. Kami akan otomatis memblokir akun dengan konten SARA, provokasi dan pornografi."
+    )
 
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username',
-                  'email', 'password1', 'password2']
+                  'email', 'password1', 'password2', 'terms']
 
 
 class UserUpdateForm(forms.ModelForm):
