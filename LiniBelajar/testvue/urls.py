3b36ee463cc6e315cb96617d416e0ff7bfbd26a7
oktoberlin <oktoberlin@gmail.com>
2021-07-08 22:45:21 +0700

    update link for vue test

--- LiniBelajar/testvue/urls.py
+++ LiniBelajar/testvue/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from testvue import views as myapp_views
 
 urlpatterns = [
-    path('vue-test', myapp_views.vue_test),
+    path('vue-test/', myapp_views.vue_test),
 ]
